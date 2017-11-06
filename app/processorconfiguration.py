9ed1973eb3d05357067b5c3b5413e1d20b6b6221
ali92hm <ali92hm@gmail.com>
2017-11-06 16:02:27 -0500

    create dir if doesnt exist

--- app/processorconfiguration.py
+++ app/processorconfiguration.py
@@ -13,13 +13,11 @@ config = Config()
 
 blob_service = BlockBlobService(account_name=config.storage_account_name, sas_token=config.encrypted_files_sas_token)
 
-try:
-    os.mkdir(config.encrypted_files_folder)
-except:
-    pass
+if not os.path.exists(config.encrypted_files_folder):
+    os.makedirs(config.encrypted_files_folder)
 
 # Download encrypted AES key
-blob_service.get_blob_to_path(container_name=config.storage_container_name, 
+blob_service.get_blob_to_path(container_name=config.storage_container_name,
                               blob_name=config.encrypted_aes_key_filename,
                               file_path=config.encrypted_files_folder + "/" + config.encrypted_aes_key_filename)
 encrypted_aes_key_filename = config.encrypted_files_folder + "/" + config.encrypted_aes_key_filename
