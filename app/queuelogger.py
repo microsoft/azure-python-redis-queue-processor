6fe9ca359d86babe47521d117847998a9a6c6adc
Arjun Jobanputra <ajobanpu@microsoft.com>
2017-11-07 14:13:06 -0800

    queuq logger reads from stdin

--- app/queuelogger.py
+++ app/queuelogger.py
@@ -4,6 +4,7 @@
 import sys
 import socket
 import json
+import fileinput
 from datetime import datetime
 from azure.storage.queue import QueueService, models
 from config import Config
@@ -12,18 +13,20 @@ class QueueLogger(object):
     """
     This class contains functionality to log stdout to Azure Queue Storage
     """
-    def __init__(self, batch_size):
+    def __init__(self):
         """
         Initializes a new instance of the QueueLogger class.
 
         :param int batch_size: The number of messages to write into a single Azure Storage Queue message.
         """
         self.config = Config()
-        self.batch_size = batch_size
         self.queue_service = QueueService(account_name =  self.config.logger_storage_account_name,
             sas_token = self.config.logger_queue_sas)
         self.queue_service.encode_function = models.QueueMessageFormat.noencode
-        self.messages_to_write = list()
+
+    def start_listening(self):
+        for line in fileinput.input():
+            self.write(line.strip())
 
     def flush(self):
         """
@@ -37,18 +40,26 @@ class QueueLogger(object):
 
         :param str content: The content to write/buffer
         """
-        if(content == '\n' or content == ''):
-            return
+        self.write_stdout(content)
+        self.write_queue(content)
 
-        self.messages_to_write.append(content)
-        if(len(self.messages_to_write) >= self.batch_size):
-            self.put_message_to_queue()
+    def write_queue(self, content):
+        """
+        Buffers string content to be written to Storage Queue
 
-    def put_message_to_queue(self):
+        :param str content: The content to write/buffer
         """
-        Adds a new Storage Queue message to the back of the message queue.
+        self.queue_service.put_message(self.config.logger_queue_name, content)
+
+    def write_stdout(self, content):
         """
-        if(len(self.messages_to_write) > 0):
-            json_content = json.dumps(self.messages_to_write,sort_keys=True,indent=4, separators=(',', ': '))
-            self.queue_service.put_message(self.config.logger_queue_name, content = json_content)
-            self.messages_to_write = list()
+        Buffers string content to be written to Storage Queue
+
+        :param str content: The content to write/buffer
+        """
+        print(content)
+
+
+if __name__ == "__main__":
+    queuelogger = QueueLogger()
+    queuelogger.start_listening()
