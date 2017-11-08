13b7d27693cb2d16d86d2d8d3f5666ad1bbcfa54
Jeff Day <jeffday@microsoft.com>
2017-11-08 09:58:26 -0500

    Update scheduler_bootstrap.sh

--- app/scheduler_bootstrap.sh
+++ app/scheduler_bootstrap.sh
@@ -9,8 +9,9 @@ pip install rq
 
 tar -xzf schedulerscripts.tar.gz
 
+echo "* * * * * python validator.py --redisHost $1 --redisPort 6379 >/dev/null 2>&1" > crontab
+/sbin/service crond start
+
 python schedulerconfiguration.py
 python scheduler-unencrypted.py data/data.encrypted --redisHost $1 --redisPort 6379
 
-echo "* * * * * python validator.py --redisHost $1 --redisPort 6379 >/dev/null 2>&1" > crontab
-/sbin/service crond start
